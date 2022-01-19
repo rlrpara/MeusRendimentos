9d49b6fa6a577379897cb4d3ea94f713c2f15543
Rodrigo Ribeiro <rlr.para@gmail.com>
2022-01-19 16:22:27 -0300

    estrutura base da api

--- MeusRendimentos/Startup.cs
+++ MeusRendimentos/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace MeusRendimentos
 {
@@ -18,7 +19,17 @@ namespace MeusRendimentos
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
-            services.AddControllers();
+            services.AddSpaStaticFiles(diretorio =>
+            {
+                diretorio.RootPath = "MeusRendimentosUi";
+            });
+            services.AddControllers()
+                .AddJsonOptions(x => {
+                    x.JsonSerializerOptions.IgnoreNullValues = true;
+                })
+                .AddNewtonsoftJson(x => {
+                    x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -28,13 +39,30 @@ namespace MeusRendimentos
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(x =>
+            {
+                x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
 
+            app.UseStaticFiles();
+
+            app.UseSpaStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpa(x =>
+            {
+                x.Options.SourcePath = Path.Combine(Directory.GetCurrentDirectory(), "MeusRendimentosUi");
+
+                if (env.IsDevelopment())
+                    x.UseProxyToSpaDevelopmentServer($"http://localhost:4200");
             });
         }
     }
