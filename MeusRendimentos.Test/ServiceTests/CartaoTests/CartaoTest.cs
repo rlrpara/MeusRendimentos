30485f4e851ae3b554c406ae5e6e3fc0aa818b59
Rodrigo Ribeiro <rlr.para@gmail.com>
2022-01-21 14:58:14 -0300

    teste base

---
+++ MeusRendimentos.Test/ServiceTests/CartaoTests/CartaoTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace MeusRendimentos.Test.ServiceTests.CartaoTests
+{
+    [Trait("Services", "CartaoService")]
+    public class CartaoTest
+    {
+        [Fact(DisplayName = "Teste Base")]
+        public void TesteBase()
+        {
+            var teste = 1;
+
+            Assert.Equal(1, teste);
+        }
+    }
+}
